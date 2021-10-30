947fade08f3a1ce3b47537583c292bdf6a653069
Edelgard <jtboyman@gmail.com>
2021-10-30 14:19:57 -0400

    can generate and save tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-$(".description").on("click", "p", function() {
-    let text = $(this).text().trim();
+var dailyTasks = [];
 
-    let textInput = $("<textarea>").val(text);
+let loadTasks = function() {
+    dailyTasks = JSON.parse(localStorage.getItem('dailyTasks'));
+    if (!dailyTasks) {    
+        dailyTasks = [
+            {id:0, text:""},
+            {id:1, text:""},
+            {id:2, text:""},
+            {id:3, text:""},
+            {id:4, text:""},
+            {id:5, text:""},
+            {id:6, text:""},
+            {id:7, text:""},
+            {id:8, text:""},
+        ]
+    };
+//figure out how to loop this
+    $.each(dailyTasks, function() {
+        
+    })
+};
 
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
+$(".saveBtn").on("click", function() {
+
+    let time = $(this).closest(".row").attr("id");
+
+    let task = $("#"+time).children("textarea").val();
+
+    console.log(time);
+    console.log(task);
+
+    dailyTasks[time].text = task;
+    localStorage.setItem("dailyTasks", JSON.stringify(dailyTasks));
 });
+
+loadTasks();
